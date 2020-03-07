c322532c64ef28aadcc0abd889ccad7c4870655f
Ali <Ali@Jalals-MacBook-Pro.local>
2020-03-07 21:35:58 +0000

    day one challenge completed

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,13 @@
+window.addEventListener("keydown", e => {
+    // console.log(e.keyCode)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    // console.log(audioElement)
+    if (audio) {
+        audio.play() 
+        const audioDiv = document.querySelector(`div[data-key="${e.keyCode}"]`)
+        audioDiv.classList.add('playing')
+        setTimeout(() => {
+            audioDiv.classList.remove('playing')
+        }, 100)
+    } 
+})
